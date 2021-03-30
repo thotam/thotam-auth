476f392754943d72db937d3058306d2d2580ee0c
thanhtamtotaa <thanhtamtqno1@gmail.com>
2021-03-30 13:50:03 +0700

    Update ThotamAuthServiceProvider.php

--- src/ThotamAuthServiceProvider.php
+++ src/ThotamAuthServiceProvider.php
@@ -16,7 +16,7 @@ class ThotamAuthServiceProvider extends ServiceProvider
          */
         // $this->loadTranslationsFrom(__DIR__.'/../resources/lang', 'thotam-auth');
         $this->loadViewsFrom(__DIR__.'/../resources/views', 'thotam-auth');
-        // $this->loadMigrationsFrom(__DIR__.'/../database/migrations');
+        $this->loadMigrationsFrom(__DIR__.'/../database/migrations');
         // $this->loadRoutesFrom(__DIR__.'/routes.php');
 
         if ($this->app->runningInConsole()) {
